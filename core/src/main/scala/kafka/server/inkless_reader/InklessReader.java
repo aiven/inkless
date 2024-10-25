cea63d1140f9a0ffd8da93746fdff692635ff1af
Ivan Yurchenko <ivanyu@aiven.io>
2024-10-25 20:36:36 +0300

    WIP - write to S3 instead of local disk

--- core/src/main/scala/kafka/server/inkless_reader/InklessReader.java
+++ core/src/main/scala/kafka/server/inkless_reader/InklessReader.java
@@ -1,7 +1,7 @@
 package kafka.server.inkless_reader;
 
-import java.io.File;
 import java.io.IOException;
+import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -11,7 +11,7 @@ import java.util.concurrent.CompletableFuture;
 
 import org.apache.kafka.common.TopicIdPartition;
 import org.apache.kafka.common.protocol.Errors;
-import org.apache.kafka.common.record.FileRecords;
+import org.apache.kafka.common.record.MemoryRecords;
 import org.apache.kafka.common.record.Records;
 import org.apache.kafka.common.requests.FetchRequest;
 import org.apache.kafka.server.storage.log.FetchPartitionData;
@@ -22,14 +22,20 @@ import kafka.server.inkless_control_plane.ControlPlane;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import scala.Tuple2;
+import software.amazon.awssdk.core.ResponseInputStream;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
+import software.amazon.awssdk.services.s3.model.GetObjectResponse;
 
 public class InklessReader {
     private static final Logger logger = LoggerFactory.getLogger(InklessReader.class);
 
     private final ControlPlane controlPlane;
+    private final S3Client s3Client;
 
-    public InklessReader(final ControlPlane controlPlane) {
+    public InklessReader(final ControlPlane controlPlane, final S3Client s3Client) {
         this.controlPlane = controlPlane;
+        this.s3Client = s3Client;
     }
 
     public CompletableFuture<Iterable<Tuple2<TopicIdPartition, FetchPartitionData>>> read(
@@ -44,15 +50,7 @@ public class InklessReader {
             ));
             logger.error("findBatchResponse={}", findBatchResponse);
 
-            final Records records;
-            try {
-                records = FileRecords.open(new File(findBatchResponse.filePath))
-                    .slice(findBatchResponse.byteOffset, findBatchResponse.byteSize);
-            } catch (final IOException e) {
-                // TODO handle
-                throw new RuntimeException(e);
-            }
-
+            final Records records = getRecords(findBatchResponse);
             final FetchPartitionData fetchPartitionData = new FetchPartitionData(
                 Errors.NONE,
                 findBatchResponse.batchBaseOffset + findBatchResponse.numberOfRecords,
@@ -68,5 +66,23 @@ public class InklessReader {
         }
 
         return CompletableFuture.completedFuture(result);
+    }
+
+    private Records getRecords(final FindBatchResponse findBatchResponse) {
+        final GetObjectRequest getObjectRequest = GetObjectRequest.builder()
+            .key(findBatchResponse.filePath)
+            .bucket("my-bucket")
+            .build();
+        final ResponseInputStream<GetObjectResponse> responseInputStream = s3Client.getObject(getObjectRequest);
+        final byte[] data;
+        try {
+            responseInputStream.skip(findBatchResponse.byteOffset);
+            data = responseInputStream.readNBytes(findBatchResponse.byteSize);
+        } catch (final IOException e) {
+            // TODO handle
+            throw new RuntimeException(e);
+        }
+
+        return MemoryRecords.readableRecords(ByteBuffer.wrap(data));
     }
 }
