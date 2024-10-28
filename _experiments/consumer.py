5fdf78fda5efd0388fc41610e2de367e2f04fb91
Ivan Yurchenko <ivanyu@aiven.io>
2024-10-28 14:50:50 +0200

    WIP - handle high watermarks in fetch

--- _experiments/consumer.py
+++ _experiments/consumer.py
@@ -3,7 +3,15 @@ from kafka import KafkaConsumer, TopicPartition
 consumer = KafkaConsumer(
     bootstrap_servers="127.0.0.1:9092",
 )
-consumer.assign([TopicPartition("topic", 0)])
+tp = TopicPartition("topic", 0)
+consumer.assign([tp])
+consumer.seek(tp, 2)
 
-r = consumer.poll(timeout_ms=1000)
-print(r)
+for i in range(5):
+    batch = consumer.poll(timeout_ms=1000)
+    print(f"Batch {i}")
+    if not batch.get(tp):
+        continue
+    for r in batch.get(tp):
+        print(r.offset, r.value)
+    print()
