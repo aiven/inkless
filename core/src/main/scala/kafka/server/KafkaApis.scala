c7ce111bc98716af73141af6118808598b44d629
Ivan Yurchenko <ivanyu@aiven.io>
2024-12-13 11:51:58 +0200

    Add metadata transformation for DescribeTopicPartitionsRequest [INK-57] (#113)

--- core/src/main/scala/kafka/server/KafkaApis.scala
+++ core/src/main/scala/kafka/server/KafkaApis.scala
@@ -949,7 +949,7 @@ class KafkaApis(val requestChannel: RequestChannel,
       }
     }
 
-    inklessTopicMetadataTransformer.transform(request.context.clientId(), topicMetadata.asJava)
+    inklessTopicMetadataTransformer.transformClusterMetadata(request.context.clientId(), topicMetadata.asJava)
 
     val completeTopicMetadata =  unknownTopicIdsTopicMetadata ++
       topicMetadata ++ unauthorizedForCreateTopicMetadata ++ unauthorizedForDescribeTopicMetadata
@@ -983,6 +983,8 @@ class KafkaApis(val requestChannel: RequestChannel,
         val response = handler.handleDescribeTopicPartitionsRequest(request)
         trace("Sending topic partitions metadata %s for correlation id %d to client %s".format(response.topics().asScala.mkString(","),
           request.header.correlationId, request.header.clientId))
+
+        inklessTopicMetadataTransformer.transformDescribeTopicResponse(request.header.clientId, response)
 
         requestHelper.sendResponseMaybeThrottle(request, requestThrottleMs => {
           response.setThrottleTimeMs(requestThrottleMs)
