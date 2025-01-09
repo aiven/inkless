21932f3f037dcca640ad8fadf32a4fe89aa396b6
Jorge Esteban Quilcate Otoya <jorge.quilcate@aiven.io>
2025-01-09 06:07:30 -0500

    Single control-plane instance [INK-81] (#138)

--- core/src/main/scala/kafka/server/BrokerServer.scala
+++ core/src/main/scala/kafka/server/BrokerServer.scala
@@ -333,8 +333,17 @@ class BrokerServer(
       val defaultActionQueue = new DelayedActionQueue
 
       val inklessMetadataView = new InklessMetadataView(metadataCache)
-      val inklessSharedState = SharedState.initialize(time, config.brokerId, config.inklessConfig, inklessMetadataView, brokerTopicStats,
-        () => logManager.currentDefaultConfig)
+      val inklessSharedState = sharedServer.inklessControlPlane.map { controlPlane =>
+        SharedState.initialize(
+          time,
+          config.brokerId,
+          config.inklessConfig,
+          inklessMetadataView,
+          controlPlane,
+          brokerTopicStats,
+          () => logManager.currentDefaultConfig
+        )
+      }
 
       this._replicaManager = new ReplicaManager(
         config = config,
@@ -355,7 +364,7 @@ class BrokerServer(
         addPartitionsToTxnManager = Some(addPartitionsToTxnManager),
         directoryEventHandler = directoryEventHandler,
         defaultActionQueue = defaultActionQueue,
-        inklessSharedState = Some(inklessSharedState)
+        inklessSharedState = inklessSharedState
       )
 
       /* start token manager */
@@ -471,7 +480,7 @@ class BrokerServer(
         tokenManager = tokenManager,
         apiVersionManager = apiVersionManager,
         clientMetricsManager = clientMetricsManager,
-        inklessSharedState = Some(inklessSharedState))
+        inklessSharedState = inklessSharedState)
 
       dataPlaneRequestHandlerPool = new KafkaRequestHandlerPool(config.nodeId,
         socketServer.dataPlaneRequestChannel, dataPlaneRequestProcessor, time,
