b85c725672eea0c39e7402b96cb62e5057b03091
Apoorv Mittal <apoorvmittal10@gmail.com>
2025-01-15 19:08:05 +0000

    KAFKA-18452: Implemented batch size in acquired records (#18459)
    The PR implements a way to divide acquired batches into batch size as desirable by client.
    
    The BatchSize is the soft limit and should align the batches in response and cached state in broker at the log batch boundaries.
    
    Reviewers:  Andrew Schofield <aschofield@confluent.io>, Abhinav Dixit <adixit@confluent.io>, Jun Rao <junrao@gmail.com>

--- core/src/main/scala/kafka/server/KafkaApis.scala
+++ core/src/main/scala/kafka/server/KafkaApis.scala
@@ -3049,6 +3049,7 @@ class KafkaApis(val requestChannel: RequestChannel,
         groupId,
         shareFetchRequest.data.memberId,
         params,
+        shareFetchRequest.data.batchSize,
         interestedWithMaxBytes
       ).thenApply{ result =>
         val combinedResult = mutable.Map.empty[TopicIdPartition, ShareFetchResponseData.PartitionData]
