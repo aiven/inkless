1c997579f44b28833839641fea47286a5c4fdd7c
Dongnuo Lyu <139248811+dongnuo123@users.noreply.github.com>
2025-02-26 13:05:36 -0500

    KAFKA-18813: ConsumerGroupHeartbeat API and ConsumerGroupDescribe API must check topic describe (#18989)
    This patch filters out the topic describe unauthorized topics from the
    ConsumerGroupHeartbeat and ConsumerGroupDescribe response.
    
    In ConsumerGroupHeartbeat, 
    - if the request has `subscribedTopicNames` set, we directly check the
    authz in `KafkaApis` and return a topic auth failure in the response if
    any of the topics is denied.
    - Otherwise, we check the authz only if a regex refresh is triggered and
    we do it based on the acl of the consumer that triggered the refresh. If
    any of the topic is denied, we filter it out from the resolved
    subscription.
    
    In ConsumerGroupDescribe, we check the authz of the coordinator
    response. If any of the topic in the group is denied, we remove the
    described info and add a topic auth failure to the described group.
    (similar to the group auth failure)
    
    Reviewers: David Jacot <djacot@confluent.io>, Lianet Magrans
    <lmagrans@confluent.io>, Rajini Sivaram <rajinisivaram@googlemail.com>,
    Chia-Ping Tsai <chia7712@gmail.com>, TaiJuWu <tjwu1217@gmail.com>,
    TengYao Chi <kitingiao@gmail.com>

--- core/src/main/scala/kafka/server/BrokerServer.scala
+++ core/src/main/scala/kafka/server/BrokerServer.scala
@@ -663,6 +663,7 @@ class BrokerServer(
         .withGroupCoordinatorMetrics(new GroupCoordinatorMetrics(KafkaYammerMetrics.defaultRegistry, metrics))
         .withGroupConfigManager(groupConfigManager)
         .withPersister(persister)
+        .withAuthorizer(authorizer.toJava)
         .build()
     } else {
       GroupCoordinatorAdapter(
