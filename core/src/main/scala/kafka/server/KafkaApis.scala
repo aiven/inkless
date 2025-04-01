9b290ef1e96d927c4037819a2b4b7562fa9dc1c3
Ivan Yurchenko <ivanyu@aiven.io>
2025-04-01 07:37:29 +0100

    Make KafkaApis work without InklessSharedState

--- core/src/main/scala/kafka/server/KafkaApis.scala
+++ core/src/main/scala/kafka/server/KafkaApis.scala
@@ -119,7 +119,7 @@ class KafkaApis(val requestChannel: RequestChannel,
   val describeTopicPartitionsRequestHandler = new DescribeTopicPartitionsRequestHandler(
     metadataCache, authHelper, config)
 
-  val inklessTopicMetadataTransformer = new InklessTopicMetadataTransformer(inklessSharedState.get.metadata())
+  val inklessTopicMetadataTransformer = inklessSharedState.map(s => new InklessTopicMetadataTransformer(s.metadata()))
 
   def close(): Unit = {
     aclApis.close()
@@ -950,7 +950,7 @@ class KafkaApis(val requestChannel: RequestChannel,
       }
     }
 
-    inklessTopicMetadataTransformer.transformClusterMetadata(request.context.clientId(), topicMetadata.asJava)
+    inklessTopicMetadataTransformer.foreach(t => t.transformClusterMetadata(request.context.clientId(), topicMetadata.asJava))
 
     val completeTopicMetadata =  unknownTopicIdsTopicMetadata ++
       topicMetadata ++ unauthorizedForCreateTopicMetadata ++ unauthorizedForDescribeTopicMetadata
@@ -978,7 +978,7 @@ class KafkaApis(val requestChannel: RequestChannel,
     trace("Sending topic partitions metadata %s for correlation id %d to client %s".format(response.topics().asScala.mkString(","),
       request.header.correlationId, request.header.clientId))
 
-    inklessTopicMetadataTransformer.transformDescribeTopicResponse(request.header.clientId, response)
+    inklessTopicMetadataTransformer.foreach(t => t.transformDescribeTopicResponse(request.header.clientId, response))
 
     requestHelper.sendResponseMaybeThrottle(request, requestThrottleMs => {
       response.setThrottleTimeMs(requestThrottleMs)
@@ -1860,7 +1860,7 @@ class KafkaApis(val requestChannel: RequestChannel,
           // Any failed partition check causes the entire transaction to fail. We send the appropriate error codes for the
           // partitions which failed, and an 'OPERATION_NOT_ATTEMPTED' error code for the partitions which succeeded
           // the authorization check to indicate that they were not added to the transaction.
-          val partitionErrors = unauthorizedTopicErrors ++ nonExistingTopicErrors ++
+          val partitionErrors = unauthorizedTopicErrors ++ nonExistingTopicErrors ++ prohibitedInklessTopicErrors ++
             authorizedPartitions.map(_ -> Errors.OPERATION_NOT_ATTEMPTED)
           addResultAndMaybeSendResponse(AddPartitionsToTxnResponse.resultForTransaction(transactionalId, partitionErrors.asJava))
         } else {
