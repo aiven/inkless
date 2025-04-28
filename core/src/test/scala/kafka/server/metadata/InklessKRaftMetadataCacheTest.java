835608a30098796ae34bfe73c983da0f0820e9b4
Greg Harris <greg.harris@aiven.io>
2025-04-28 15:35:53 -0700

    fixup: add more test cases to InklessKRaftMetadataCacheTest
    Signed-off-by: Greg Harris <greg.harris@aiven.io>

--- core/src/test/scala/kafka/server/metadata/InklessKRaftMetadataCacheTest.java
+++ core/src/test/scala/kafka/server/metadata/InklessKRaftMetadataCacheTest.java
@@ -45,22 +45,47 @@ class InklessKRaftMetadataCacheTest {
 
     @ParameterizedTest
     @CsvSource({
-        "__consumer_offsets,false", "__transaction_state,false", "__share_group_state,false", "__cluster_metadata,false",
-        "__other_topic1,false", "__other_topic2__,true", "_other_topic3,false", "other_topic4,true"})
-    void isInklessTopic(final String topicName, final boolean expectedIsInkless) {
-        Supplier<Map<?, ?>> defaultConfig = () -> CollectionConverters.asScala(Collections.emptyMap());
+        "__consumer_offsets,false,false", "__consumer_offsets,true,false",
+        "__transaction_state,false,false", "__transaction_state,true,false",
+        "__share_group_state,false,false", "__share_group_state,true,false",
+        "__cluster_metadata,false,false", "__cluster_metadata,true,false",
+        "__internal_topic_default,false,false", "__internal_topic_default,true,true",
+        "__internal_topic_enabled,false,true", "__internal_topic_enabled,true,true",
+        "__internal_topic_disabled,false,false", "__internal_topic_disabled,true,false",
+        "regular_topic_default,false,false", "regular_topic_default,true,true",
+        "regular_topic_enabled,false,true", "regular_topic_enabled,true,true",
+        "regular_topic_disabled,false,false", "regular_topic_disabled,true,false",
+    })
+    void isInklessTopic(final String topicName, final boolean defaultInklessEnable, final boolean expectedIsInkless) {
+        Supplier<Map<?, ?>> defaultConfig = () -> CollectionConverters.asScala(
+            defaultInklessEnable ?
+                Collections.singletonMap(TopicConfig.INKLESS_ENABLE_CONFIG, "true") :
+                Collections.emptyMap()
+        );
         // Given a cache with a couple of inkless topics
         final KRaftMetadataCache cache = MetadataCache.kRaftMetadataCache(1, () -> KRaftVersion.KRAFT_VERSION_0);
-        final List<ApiMessage> configRecords = List.of(new ConfigRecord()
+        final List<ApiMessage> configRecords = List.of(
+            new ConfigRecord()
                 .setResourceType(ResourceType.TOPIC.code())
-                .setResourceName("__other_topic2__")
+                .setResourceName("__internal_topic_enabled")
                 .setName(TopicConfig.INKLESS_ENABLE_CONFIG)
                 .setValue("true"),
             new ConfigRecord()
                 .setResourceType(ResourceType.TOPIC.code())
-                .setResourceName("other_topic4")
+                .setResourceName("__internal_topic_disabled")
                 .setName(TopicConfig.INKLESS_ENABLE_CONFIG)
-                .setValue("true"));
+                .setValue("false"),
+            new ConfigRecord()
+                .setResourceType(ResourceType.TOPIC.code())
+                .setResourceName("regular_topic_enabled")
+                .setName(TopicConfig.INKLESS_ENABLE_CONFIG)
+                .setValue("true"),
+            new ConfigRecord()
+                .setResourceType(ResourceType.TOPIC.code())
+                .setResourceName("regular_topic_disabled")
+                .setName(TopicConfig.INKLESS_ENABLE_CONFIG)
+                .setValue("false")
+        );
         updateCache(cache, configRecords);
         // When checking if a topic is inkless, then the expected result is returned
         assertEquals(expectedIsInkless, cache.isInklessTopic(topicName, defaultConfig));
