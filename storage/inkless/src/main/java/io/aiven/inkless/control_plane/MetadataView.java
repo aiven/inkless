059cfd725726f555cb1ef4f781bd94b638677025
Ivan Yurchenko <ivanyu@aiven.io>
2025-06-09 14:35:28 +0300

    Implement retention enforcement in broker [INK-251]

--- storage/inkless/src/main/java/io/aiven/inkless/control_plane/MetadataView.java
+++ storage/inkless/src/main/java/io/aiven/inkless/control_plane/MetadataView.java
@@ -18,20 +18,25 @@
 package io.aiven.inkless.control_plane;
 
 import org.apache.kafka.admin.BrokerMetadata;
-import org.apache.kafka.common.TopicPartition;
+import org.apache.kafka.common.TopicIdPartition;
 import org.apache.kafka.common.Uuid;
 
+import java.util.Map;
 import java.util.Properties;
 import java.util.Set;
 
 public interface MetadataView {
+    Map<String, Object> getDefaultConfig();
+
     Iterable<BrokerMetadata> getAliveBrokers();
 
-    Set<TopicPartition> getTopicPartitions(String topicName);
+    Integer getBrokerCount();
 
     Uuid getTopicId(String topicName);
 
     boolean isInklessTopic(String topicName);
 
     Properties getTopicConfig(String topicName);
+
+    Set<TopicIdPartition> getInklessTopicPartitions();
 }
