069baaa0ea20c3db5752a83ce53efcee6ced8bb9
Jorge Esteban Quilcate Otoya <jorge.quilcate@aiven.io>
2025-08-13 16:14:00 +0300

    refactor(inkless): avoid try complete on inkless append
    Given the current design, control plane may be overloaded by findFetch
    requests as part of the tryComplete.
    To avoid this, DelayedFetch operations are only tried to complete on
    classic append results, and on timeout.

--- core/src/main/scala/kafka/server/ReplicaManager.scala
+++ core/src/main/scala/kafka/server/ReplicaManager.scala
@@ -726,18 +726,8 @@ class ReplicaManager(val config: KafkaConfig,
           error("Inkless append future failed", e)
           inklessEntries.map{ case (tp, _) => tp -> new PartitionResponse(Errors.UNKNOWN_SERVER_ERROR)}
         }
-        val inklessAppendResults = new mutable.HashMap[TopicIdPartition, LogAppendResult]
-        inklessResult.foreach {
-          case (tp, _) =>
-            // Consider every successful produce request as if it increased the high watermark. This is a simplification
-            // that will always result in the attempt to complete the delayed operations.
-            // The current implementation does not take into account that other brokers might have performed appends
-            // that would unblock delayed operations in the purgatories of this broker.
-            val logAppendInfo = LogAppendInfo.UNKNOWN_LOG_APPEND_INFO.copy(LeaderHwChange.INCREASED)
-            val logAppendResult =  LogAppendResult(logAppendInfo, None, hasCustomErrorMessage = false)
-            inklessAppendResults += (tp -> logAppendResult)
-        }
-        addCompletePurgatoryAction(this.defaultActionQueue, inklessAppendResults)
+        // Inkless append results do not complete purgatory actions to avoid overloading the control-plane.
+        // only classic append results complete purgatory actions.
         responseCallback(inklessResult ++ classicResult)
       }
     }
