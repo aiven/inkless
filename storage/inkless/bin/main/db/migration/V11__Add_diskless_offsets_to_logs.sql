5fa25e4d9e0149d0d01d4b423a766a0fa0a16732
Giuseppe Lillo <giuseppe.lillo@aiven.io>
2026-01-09 16:43:06 +0100

    a

--- storage/inkless/bin/main/db/migration/V11__Add_diskless_offsets_to_logs.sql
+++
@@ -1,3 +0,0 @@
--- Copyright (c) 2024-2025 Aiven, Helsinki, Finland. https://aiven.io/
-ALTER TABLE logs ADD COLUMN diskless_start_offset offset_nullable_t DEFAULT NULL;
-ALTER TABLE logs ADD COLUMN diskless_end_offset offset_nullable_t DEFAULT NULL;
