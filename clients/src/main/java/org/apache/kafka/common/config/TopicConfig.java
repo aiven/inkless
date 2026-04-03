d477758cbc6de03cec6b23793773fbda8e96b8eb
Federico Valeri <fedevaleri@gmail.com>
2026-04-03 11:47:40 +0200

    Minor update to the mirror.name desc
    Signed-off-by: Federico Valeri <fedevaleri@gmail.com>

--- clients/src/main/java/org/apache/kafka/common/config/TopicConfig.java
+++ clients/src/main/java/org/apache/kafka/common/config/TopicConfig.java
@@ -222,7 +222,7 @@ public class TopicConfig {
     public static final String MIRROR_NAME_CONFIG = "mirror.name";
     public static final String MIRROR_NAME_DOC = "Identifies the mirror that manages this topic. " +
             "Topics with this configuration set are read-only and can only be modified through mirror management APIs. " +
-            "This property is hidden from DescribeConfigs responses to avoid exposing internal state to users.";
+            "This property is filtered out from DescribeConfigs responses to avoid exposing internal state to users.";
 
     /**
      * @deprecated down-conversion is not possible in Apache Kafka 4.0 and newer, hence this configuration is a no-op,
