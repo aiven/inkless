7133232a9b0a92ce580525d51e439eb5261fd5bf
Federico Valeri <fedevaleri@gmail.com>
2026-04-10 17:00:15 +0200

    Make checkstyle happy
    Signed-off-by: Federico Valeri <fedevaleri@gmail.com>

--- core/src/main/java/kafka/server/mirror/MirrorMetadataManager.java
+++ core/src/main/java/kafka/server/mirror/MirrorMetadataManager.java
@@ -110,10 +110,10 @@ import org.apache.kafka.server.util.RequestAndCompletionHandler;
 import org.slf4j.Logger;
 
 import java.net.InetAddress;
-import java.util.Collections;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
