c67b1ee3f729cf225f6424a002c0f67507fe55c3
Federico Valeri <fedevaleri@gmail.com>
2026-04-29 13:44:12 +0200

    Update KafkaAdminClient.java
    Since these are controller-bound requests, startMirrorTopics should also use LeastLoadedBrokerOrActiveKController() for efficiency and consistency.

--- clients/src/main/java/org/apache/kafka/clients/admin/KafkaAdminClient.java
+++ clients/src/main/java/org/apache/kafka/clients/admin/KafkaAdminClient.java
@@ -4922,7 +4922,7 @@ public class KafkaAdminClient extends AdminClient {
         final KafkaFutureImpl<Void> future = new KafkaFutureImpl<>();
         final long now = time.milliseconds();
         final Call call = new Call("startMirrorTopics", calcDeadlineMs(now, options.timeoutMs()),
-                new LeastLoadedNodeProvider()) {
+                new LeastLoadedBrokerOrActiveKController()) {
 
             @Override
             StartMirrorTopicsRequest.Builder createRequest(int timeoutMs) {
